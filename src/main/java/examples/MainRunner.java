4d8aba192712d58ee9364af7a31d9b8bdbf95042
rolandovillca@gmail.com <rolandovillca@gmail.com>
2016-09-25 15:40:31 -0400

    Fix avoid to use application plugin to run tasks

--- src/main/java/examples/MainRunner.java
+++ src/main/java/examples/MainRunner.java
@@ -4,6 +4,6 @@ public class MainRunner {
 
     // Start method:
     public static void main(String[] args) {
-        System.out.println("Hello Awesome Rolando");
+        System.out.println("Hello Awesome Rolando!");
     }
 }
