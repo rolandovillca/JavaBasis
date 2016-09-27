f12768ec41585e9582eaca3c3bb0bc87298b9fe1
rolandovillca@gmail.com <rolandovillca@gmail.com>
2016-09-27 09:08:56 -0400

    Adding renamed classes and fix its reference calls

---
+++ src/main/java/com/estelisa/algorithms/MathOperation.java
@@ -0,0 +1,51 @@
+package com.estelisa.algorithms;
+
+public class MathOperation {
+
+    /*
+     * Given an array of integers, find two numbers such that they add up to a specific target number.
+     * The function twoSum should return indices of the two numbers such that they add up to the target,
+     * where index1 must be less than index2.
+     *
+     * Please note that your returned answers (both index1 and index2) are not zero-based.
+     * For example:
+        Input: numbers={2, 7, 11, 15}, target=9
+        Output: index1=1, index2=2
+     */
+    public int twoSum(int[] numbers) {
+        int index = 0;
+        return index;
+    }
+
+    /*
+     * Write a function that accepts an array of non-negative integers and returns the second largest integer in the array.
+     * Return -1 if there is no second largest. You may assume that the input array has no negative values in it.
+     *
+     * If you are programming in Java or C#, the signature of the function is int f(int[ ] a)
+     * If you are programming in C or C#, the signature of the function is
+     * int f(int a[ ], int len) where len is the number of elements in a.
+     *
+     * Examples:
+     * input array       return
+     * {1, 2, 3, 4}           3
+     * {{4, 1, 2, 3}}         3
+     * {1, 1, 2, 2}           1
+     * {1, 1}                -1
+     * {1}                   -1
+     * {}                    -1
+     */
+    public int secondLargestInteger() {
+        int secondLargestInt = 0;
+        return secondLargestInt;
+    }
+
+    /*
+     * Rotate an array of n elements to the right by k steps.
+     * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+     * How many different ways do you know to solve this problem?
+     */
+    public int[] rotateArrayNElementsToRight() {
+        // int[] rotateArray = new int[10];
+        return null;
+    }
+}
---
+++ src/main/java/com/estelisa/algorithms/Searching.java
@@ -0,0 +1,29 @@
+package com.estelisa.algorithms;
+
+public class Searching {
+
+    /*
+     * Given a sorted array and a target value, return the index if the target is found.
+     * If not, return the index where it would be if it were inserted in order.
+     * You may assume no duplicates in the array.
+     * Here are few examples.
+     *
+     * [1,3,5,6], 5 -> 2
+     * [1,3,5,6], 2 -> 1
+     * [1,3,5,6], 7 -> 4
+     * [1,3,5,6], 0 -> 0
+     */
+    public void searchInsertPosition() {
+
+    }
+
+    /*
+     * Given a sorted array of integers, find the starting and ending position of a given target value.
+     * Your algorithm's runtime complexity must be in the order of O(log n).
+     * If the target is not found in the array, return [-1, -1].
+     * For example, given [5, 7, 7, 8, 8, 10] and target value 8, return [3, 4].
+     */
+    public void searchForRange() {
+
+    }
+}
